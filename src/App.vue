699cf6f7287e57db4bb1846eb0ebf206a7786939
Maksos <maximik2011@gmail.com>
2022-07-27 18:12:01 +0300

    test trois.js s

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Renderer ref="renderer" resize="window" orbit-ctrl>
+    <Camera :position="{ z: 10 }" />
+    <Scene background="#fff">
+      <AmbientLight color="#aaaaaa" />
+      <PointLight :position="{ y: 100, z: 0 }" />
+      <GltfModel ref="box" src="../public/adamHead.gltf" />
+    </Scene>
+  </Renderer>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // const renderer = this.$refs.renderer;
+    // const box = this.$refs.box.mesh;
+    // renderer.onBeforeRender(() => {
+    //   box.rotation.x += 0.01;
+    // });
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { TroisJSVuePlugin } from 'troisjs';
+
+
+const app = createApp(App)
+
+app.use(TroisJSVuePlugin);
+
+app.mount('#app')
